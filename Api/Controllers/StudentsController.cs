e17869cbc47730c2358533e8e4f2d89ac99bffb8
Aytajrajab <aytajir@code.edu.az>
2022-02-03 16:55:00 +0400

    initial commit

---
+++ Api/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DomainModels.Dtos;
+using DomainModels.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repository.DAL;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public StudentsController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var students = await _context.Students.ToListAsync();
+            
+
+            return Ok(_mapper.Map<List<StudentDto>>(students));
+        }
+    }
+}
